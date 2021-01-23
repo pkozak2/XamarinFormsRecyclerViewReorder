d451e5bf33e7498fddfd6d56695239e1626f0182
Piotr Kozak <piotr.k1992@gmail.com>
2021-01-23 21:19:41 +0100

    custom collectionview

---
+++ XamarinFormsRecyclerReorder/Controls/CustomCollectionView.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XamarinFormsRecyclerReorder.Controls
+{
+    public class CustomCollectionView : CollectionView
+    {
+    }
+}
